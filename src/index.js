a537e95aacd1152358142a9b042483829acdfe83
tsungtingdu <tsungtingdu@gmail.com>
2020-07-23 23:24:20 +0800

    create open and close modal actions

--- src/index.js
+++ src/index.js
@@ -5,9 +5,29 @@ const menuLinks = document.querySelectorAll('.menu__list__link')
 const menuToggle = document.querySelector('.navbar-toggle')
 const closeMenu = e => {
   menuToggle.checked = false
-  console.log(e.target)
-  console.log(e.menuToggle)
 }
 menuLinks.forEach(i => {
   i.addEventListener('click', closeMenu)
+})
+
+// modal control
+// open modal
+const projects = document.querySelectorAll('.case')
+const openModal = e => {
+  let id = e.target.dataset.id
+  const modal = document.querySelector(`#${id}`)
+  modal.classList.remove('d-none')
+}
+
+projects.forEach(i => {
+  i.addEventListener('click', openModal)
+})
+// close modal
+const closeIcon = document.querySelectorAll('.close__icon')
+const closeModal = e => {
+  const modal = e.target.parentNode.parentNode.parentNode
+  modal.classList.add('d-none')
+}
+closeIcon.forEach(i => {
+  i.addEventListener('click', closeModal)
 })
