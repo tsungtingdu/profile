ccc06c13c61673b57ba7c46bbf4ae4a36a3f99e2
tsungtingdu <tsungtingdu@gmail.com>
2020-07-23 14:11:54 +0800

    create menu button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './scss/main.scss'
 
-console.log('project init')
+// close menu after click
+const menuLinks = document.querySelectorAll('.menu__list__link')
+const menuToggle = document.querySelector('.navbar-toggle')
+const closeMenu = e => {
+  menuToggle.checked = false
+  console.log(e.target)
+  console.log(e.menuToggle)
+}
+menuLinks.forEach(i => {
+  i.addEventListener('click', closeMenu)
+})
